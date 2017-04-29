ec3a014a0a14df9e44b1595a1e9f2c7266835ef2
Erik Melton <meltonaerik@gmail.com>
2017-04-28 18:25:34 -0700

    Added notes view

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,4 @@
 class NotesController < ApplicationController
+  def index
+  end
 end
--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class NotesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get notes_index_url
+    assert_response :success
+  end
+
 end
