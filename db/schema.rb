37d02b5d757d69746588d363c05fbe0fba2018df
Erik Melton <meltonaerik@gmail.com>
2017-04-28 18:34:50 -0700

    Added proper columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429011523) do
+ActiveRecord::Schema.define(version: 20170429013126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170429011523) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "author"
+    t.string   "type"
   end
 
 end
