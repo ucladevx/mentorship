c59fce23f5269a8023d5f84150aeeb73b50c6570
Erik Melton <meltonaerik@gmail.com>
2017-05-10 12:32:09 -0700

    no one saw anything

--- db/schema.rb
+++ db/schema.rb
@@ -20,29 +20,29 @@ ActiveRecord::Schema.define(version: 20170510190747) do
     t.string   "uclaclass"
     t.integer  "reputation"
     t.string   "ufile"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "author"
     t.string   "notetype"
-    t.string   "usertype",         default: "s"
-    t.string   "username"
-    t.integer  "ratingscore",      default: 0
-    t.string   "uploadednoteurls", default: [],               array: true
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",  null: false
+    t.string   "encrypted_password",     default: "",  null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,   null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "usertype",               default: "s"
+    t.string   "username"
+    t.integer  "ratingscore",            default: 0
+    t.string   "uploadednoteurls",       default: [],               array: true
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
