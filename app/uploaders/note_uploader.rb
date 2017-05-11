8fd9cb5625268e4a8311f23be55a9153340c29a5
Erik Melton <meltonaerik@gmail.com>
2017-05-11 10:50:52 -0700

    adf

--- app/uploaders/note_uploader.rb
+++ app/uploaders/note_uploader.rb
@@ -36,8 +36,6 @@ class NoteUploader < CarrierWave::Uploader::Base
   #   process :resize_to_fit => [50, 50]
   # end
 
-  Add a white list of extensions which are allowed to be uploaded.
-  For images you might use something like this:
   def extension_white_list
     %w(pdf)
   end
