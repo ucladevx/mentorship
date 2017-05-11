dc4b8acbd3ebfca7310634cf58ea0d0777627150
Erik Melton <meltonaerik@gmail.com>
2017-05-11 10:46:44 -0700

    restrict files to .pdf

--- app/uploaders/note_uploader.rb
+++ app/uploaders/note_uploader.rb
@@ -36,11 +36,11 @@ class NoteUploader < CarrierWave::Uploader::Base
   #   process :resize_to_fit => [50, 50]
   # end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  Add a white list of extensions which are allowed to be uploaded.
+  For images you might use something like this:
+  def extension_white_list
+    %w(pdf)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
