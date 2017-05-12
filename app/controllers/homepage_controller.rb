6ae97e87f675daee5e54bb954964c0de8f7973f3
Jsfuentes <jsjfuentes@g.ucla.edu>
2017-05-11 21:33:13 -0700

    Login can now be global

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -6,4 +6,17 @@ class HomepageController < ApplicationController
         @user = User.find_by id:current_user.id
       end
   end
+
+  helper_method :resource_name, :resource, :devise_mapping
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
