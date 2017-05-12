3421dec7a97bf9b699d894e5a1be555622f5865b
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-05-12 04:05:19 -0700

    fixed login modal on all pages w/ turbolinks load

--- app/assets/javascripts/handlers.js
+++ app/assets/javascripts/handlers.js
@@ -1,7 +1,9 @@
-function onReady(){
+//to aggregate all code required on document.ready()
+
+var onReady = function(){
 
 	homepage();
+	initModal();
 
 }
-
-$(document).ready(onReady);
+$(document).on('turbolinks:load', onReady);
