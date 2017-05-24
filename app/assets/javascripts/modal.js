a3de795878fc8d0451a8d23a24fecf0c8f89c4d1
Charlotte McGinn <charlotte.mcginn@engineering.ucla.edu>
2017-05-24 16:31:00 -0700

    profile modal
    appears, but does not disappear yet

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -11,9 +11,11 @@ function showModal(modal){
 		case 'sign-up':
 			$('.sign-up-modal-container').removeClass('hide');
 			break;
+		case 'profile':
+			$('.profile-modal-container').removeClass('hide');
+			break;
 
 	}
-
 	
 	$('body').addClass('locked');
 }
@@ -29,6 +31,9 @@ function hideModal(modal){
 			break;
 		case 'sign-up':
 			$('.sign-up-modal-container').addClass('hide');
+			break;
+		case 'profile':
+			$('.profile-modal-container').addClass('hide');
 			break;
 		case 'all':
 			$('.modal-container').addClass('hide');
@@ -56,6 +61,8 @@ var initModal = function(){
 		hideModal('all');
 		showModal('sign-up');
 	});
-
-
+	$('.profile-trigger-modal').click(function(){
+		hideModal('all');
+		showModal('profile');
+	});
 };
