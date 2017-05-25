7a7835adccc76f347d9bc55a02b3106b1de1cf00
Austin <austindlee997@gmail.com>
2017-05-25 11:46:29 -0700

    does not show mentors on homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :resource_name, :resource, :devise_mapping
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def resource_name
     :user
   end
@@ -12,4 +14,11 @@ class ApplicationController < ActionController::Base
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
 end
