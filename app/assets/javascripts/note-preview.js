dc1b8c92b1078aa7d53a9c4a4a818095f30e630f
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-05-29 02:57:20 -0700

    added note preview on page

---
+++ app/assets/javascripts/note-preview.js
@@ -0,0 +1,34 @@
+
+
+/*
+@param string is in the format '[id num]-descriptor-of-element'
+@param delim is typically '-', used to delim the string
+*/
+function getId(string, delim){
+	return string.substring(0, string.indexOf(delim));
+}
+
+function showPreview(e){
+
+	var id = getId($(e.target)[0].id, '-');
+
+	var preview = $('#' + id + '-preview');
+	preview.removeClass('hide');
+
+}
+
+function hidePreview(e){
+
+	var id = getId($(e.target)[0].id, '-');
+
+	var preview = $('#' + id + '-preview');
+	preview.addClass('hide');
+
+}
+
+var initNotePreview = function(){
+
+	$('.trigger-preview-on').click(showPreview);
+	$('.trigger-preview-off').click(hidePreview);
+
+}
