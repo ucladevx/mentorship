eda96ef3f981e64aeb443a50ed6da3b59df1fd71
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-06-01 10:22:56 -0700

    fixed double js include issue

--- app/assets/javascripts/handlers.js
+++ app/assets/javascripts/handlers.js
@@ -1,23 +1,17 @@
 //to aggregate all code required on document.ready()
 
-var initReady = false;
+
 var onReady = function(){
-	if(!initReady){
-		homepage();
-		initModal();
-		initMentors();
-		initNewNoteModal();
-		initNotePreview();
-		initReady = true;
-	}
+	homepage();
+	initModal();
+	initMentors();
+	initNewNoteModal();
+	initNotePreview();
 };
 
 var initBody = false;
 var onBodyClick = function(e){
-	if(!initBody){
-		initNav(e);
-		initBody = true;
-	}
+	initNav(e);
 }
 
 
