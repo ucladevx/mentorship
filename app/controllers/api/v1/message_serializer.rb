6403863ff4ecda5d518a714e13e5340cc841780e
Erik Melton <meltonaerik@gmail.com>
2017-06-01 11:35:34 -0700

    More or less working conversations

--- app/controllers/api/v1/message_serializer.rb
+++ app/controllers/api/v1/message_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::MessageSerializer < Api::V1::BaseSerializer
-    attributes :id, :body, :conversation_id, :user_id
+    attributes :id, :body, :conversation_id, :user_id, :message_time
 
     def created_at
         object.created_at.in_time_zone.iso8601 if object.created_at
