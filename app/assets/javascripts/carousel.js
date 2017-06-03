3cb175880e1a8e0db729ce6542f466eedbcb9fe7
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-06-03 04:38:15 -0700

    configured carousel for skills page

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -2,39 +2,42 @@
 function initCarousel(selector, config){
 
 	var defaults = {
+		numToShow: [3, 2, 1],
 		prevArrow: '.prev',
 		nextArrow: '.next'
 	};
 
-	defaults = Object.assign(defaults, config);
+	var carouselConfig = Object.assign(defaults, config);
+
+	console.log(selector + ', ' + carouselConfig.numToShow)
 
 	$(selector).not('.slick-initialized').slick({
-		slidesToShow: 3,
-		slidesToScroll: 3,
+		slidesToShow: carouselConfig.numToShow[0],
+		slidesToScroll: carouselConfig.numToShow[0],
 		infinite: false,
 		arrows: true,
-		prevArrow: $(defaults.prevArrow),
-		nextArrow: $(defaults.nextArrow),
+		prevArrow: $(carouselConfig.prevArrow),
+		nextArrow: $(carouselConfig.nextArrow),
 		responsive: [
 		{
 			breakpoint: 1440,
 			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
+				slidesToShow: carouselConfig.numToShow[0],
+				slidesToScroll: carouselConfig.numToShow[0]
 			}
 		},
 		{
 			breakpoint: 1000,
 			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
+				slidesToShow: carouselConfig.numToShow[1],
+				slidesToScroll: carouselConfig.numToShow[1]
 			}
 		},
 		{
 			breakpoint: 700,
 			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
+				slidesToShow: carouselConfig.numToShow[2],
+				slidesToScroll: carouselConfig.numToShow[2]
 			}
 		}
 	]});
