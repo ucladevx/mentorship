6f1ad73a67b9d4665881c6a5813bb09dcc2c780e
Jsfuentes <jsjfuentes@g.ucla.edu>
2017-06-07 15:21:23 -0700

    Questions stay green if every answered

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,11 @@ class Skill < ApplicationRecord
   #if you destroy a skill you will destroy its associated concepts
   has_many :concept, dependent: :destroy
 
-  def progress
-    return 65
-  end
+  def progress(s_id)
+    if user_signed_in?
+      @user = User.find_by id:current_user.id
+      return 50
+    else
+      return 0
+    end
 end
