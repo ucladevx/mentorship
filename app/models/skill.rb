6b49054ca623a3533eadad56d2c08775bacbd9df
Jsfuentes <jsjfuentes@g.ucla.edu>
2017-06-07 15:46:39 -0700

    Add progress dynamically

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,11 +2,26 @@ class Skill < ApplicationRecord
   #if you destroy a skill you will destroy its associated concepts
   has_many :concept, dependent: :destroy
 
-  def progress(s_id)
-    if user_signed_in?
-      @user = User.find_by id:current_user.id
-      return 50
-    else
-      return 0
+  def progress(u_id)
+    unless u_id.nil?
+      user = User.find_by id:u_id
+      skill_concepts = self.concept
+      skill_questions = []
+      skill_concepts.each { |c|
+        c.question.each{ |q|
+          skill_questions.push(q.id)
+        }
+      }
+      user_completed_questions = 0
+      skill_questions.each{ |q|
+        if user.progress.include?(q)
+          user_completed_questions += 1
+        end
+      }
+      return user_completed_questions *100 / skill_questions.length
     end
+
+    return 0
+  end
+
 end
