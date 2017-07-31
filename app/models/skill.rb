ec5d70df46f864f8c2965dba15a44af62b86f4a3
Jsfuentes <jsjfuentes@g.ucla.edu>
2017-07-30 22:12:28 -0700

    Use User.class_progress("CS 31") to get the progress for a class

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,17 +2,22 @@ class Skill < ApplicationRecord
   #if you destroy a skill you will destroy its associated concepts
   has_many :concept, dependent: :destroy
 
+  def questions()
+    skill_questions = []
+    self.concept.each { |c|
+      c.question.each{ |q|
+        skill_questions.push(q.id)
+      }
+    }
+    return skill_questions
+  end
+
   def progress(u_id)
     unless u_id.nil?
       user = User.find_by id:u_id
       skill_concepts = self.concept
-      skill_questions = []
-      skill_concepts.each { |c|
-        c.question.each{ |q|
-          skill_questions.push(q.id)
-        }
-      }
       user_completed_questions = 0
+      skill_questions = self.questions()
       skill_questions.each{ |q|
         if user.progress.include?(q)
           user_completed_questions += 1
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,16 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                          format: { with: UCLA_EMAIL_REGEX }
 
-  def skill_progress(class_name)
+  def class_progress(class_name)
+    skills = Skill.all.classes(class_name)
+    if skills.blank?
+      return 0
+    end
+    total_progress = 0
+    skills.each{ |s|
+      total_progress += s.progress(self.id)
+    }
+    return ((total_progress*100.0) / (skills.length * 100.0)).round
   end
+
 end
