a2989adf6844867270c326b3f883fd16f6b23dbf
Jsfuentes <jsjfuentes@g.ucla.edu>
2017-09-19 00:33:58 -0700

    Precompile homepage.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( homepage.js )
+Rails.application.config.assets.precompile += %w( _homepage.js.erb )
 Rails.application.config.assets.precompile += %w( profile.js )
 Rails.application.config.assets.precompile += %w( prism.css )
 # Add additional assets to the asset load path
