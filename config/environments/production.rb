14bb0c23595cc46669b165944568d3fa1502b4f1
Jorge Fuentes <jsjfuentesj@yahoo.com>
2017-10-17 16:54:58 -0700

    Try 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,10 +6,10 @@ Rails.application.configure do
   Rails.application.routes.default_url_options[:host] = 'infinite-hamlet-42082.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
